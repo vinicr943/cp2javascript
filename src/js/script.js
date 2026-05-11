8557bb3da671f317a8e5865bb65cd7378807455b
Vinícius Campestrini <vinicr3009@gmail.com>
2026-05-11 20:41:40 -0300

    feat: adicionando script da loja

--- src/js/script.js
+++ src/js/script.js
@@ -48,3 +48,41 @@ function mostrarProdutos() {
 }
 
 mostrarProdutos();
+
+const container = document.getElementById("carrinho-container");
+const totalElemento = document.getElementById("total");
+
+const carrinho = [
+    { nome: "Batman", preco: 199.90, imagem: "../src/assets/img/batman.jpg" },
+    { nome: "Homem-Aranha", preco: 249.90, imagem: "../src/assets/img/homem-aranha.jpg" },
+    { nome: "Homem de Ferro", preco: 299.90, imagem: "../src/assets/img/homem-ferro.jpg" }
+];
+
+function mostrarCarrinho() {
+
+    const html = carrinho.map(item => `
+        <div class="card">
+            <img src="${item.imagem}">
+            <h3>${item.nome}</h3>
+            <p>R$ ${item.preco}</p>
+        </div>
+    `).join("");
+
+    container.innerHTML = html;
+}
+
+function calcularTotal() {
+
+    const total = carrinho.reduce((acc, item) => acc + item.preco, 0);
+
+    totalElemento.innerText = `Total: R$ ${total.toFixed(2)}`;
+}
+
+function aplicarDesconto() {
+
+    const total = carrinho.reduce((acc, item) => acc + item.preco, 0) * 0.9;
+
+    totalElemento.innerText = `Total com desconto: R$ ${total.toFixed(2)}`;
+}
+
+mostrarCarrinho();
